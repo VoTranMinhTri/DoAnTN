d188eaea5f0bcd8234e0f5118da2171e5c633b9c
VoTranMinhTri <0306191182@caothang.edu.vn>
2021-12-12 11:12:35 +0700

    fix mixcontent

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ URL::to('assets/css/style.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ URL::to('assets/css/all.css') }}">
+    <link rel="stylesheet" href="https://t-t-mobile.herokuapp.com/assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="https://t-t-mobile.herokuapp.com/assets/css/all.css">
     <title>Đăng nhập</title>
 </head>
 
